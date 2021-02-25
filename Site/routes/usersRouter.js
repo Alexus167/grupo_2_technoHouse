4fff0cdd6cf9fd85b63d7a9424f0d3298090f549
Alexus167 <alexivanmendez08@gmail.com>
2021-02-25 11:59:49 -0300

    Sprint 3

--- Site/routes/usersRouter.js
+++ Site/routes/usersRouter.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const {iniciar, registro}=require('../controller/userController');
+const {iniciar,processIniciar, registro, processRegistro}=require('../controller/userController');
 /* GET users listing. */
 
 router.get('/iniciar',iniciar);
+router.post('/iniciar',processIniciar);
+
 router.get('/registro', registro);
+router.post('/registro'.processRegistro);
+
 
 
 module.exports = router;
